dc6ad8450c99bcd740ff9aa8ea50918f89b9bcec
Kovács Vince <vincekovacs@hotmail.com>
2015-03-13 00:48:20 +0100

    Added phpDoc to helper functions

--- src/helpers.php
+++ src/helpers.php
@@ -2,14 +2,28 @@
 
 if (!function_exists('user_has_role')) {
 
-    function user_has_role($name) {
-        return app('auth.role.user')->hasRole($name);
+    /**
+     * Check if current user has single role
+     *
+     * @param string $role role name
+     *
+     * @return bool
+     */
+    function user_has_role($role) {
+        return app('auth.role.user')->hasRole($role);
     }
 }
 
 if (!function_exists('user_has_roles')) {
 
-    function user_has_roles($name) {
-        return app('auth.role.user')->hasRoles($name);
+    /**
+     * Check if current user has all roles passed as parameter
+     *
+     * @param array|string $roles roles name array
+     *
+     * @return bool
+     */
+    function user_has_roles() {
+        return app('auth.role.user')->hasRoles(func_get_args());
     }
 }
