268a3117b738f87b3dda4a6895637eb382c11a40
Kovács Vince <vincekovacs@hotmail.com>
2015-08-09 22:08:46 +0200

    Authorization helper trait for form request

---
+++ src/ViKon/Auth/Helper/FormRequestRouteAuthorizer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ViKon\Auth\Helper;
+
+use Illuminate\Routing\Router;
+use ViKon\Auth\RouterAuth;
+
+/**
+ * Class FormRequestRouteAuthorizer
+ *
+ * @package ViKon\Auth\Helper
+ *
+ * @author  Kovács Vince<vincekovacs@hotmail.com>
+ */
+trait FormRequestRouteAuthorizer
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function authorize(Router $router)
+    {
+        /** @noinspection PhpUndefinedFieldInspection */
+        return $this->container->make(RouterAuth::class)->hasAccess($router->current()->getName());
+    }
+}
