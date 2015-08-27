b5413e6f6fa3b2cca86e895e6bd890e612aff0eb
Kovács Vince <vincekovacs@hotmail.com>
2015-08-27 22:02:51 +0200

    Get user roles from Guard

--- src/ViKon/Auth/Guard.php
+++ src/ViKon/Auth/Guard.php
@@ -13,7 +13,10 @@ use ViKon\Auth\Model\User;
  */
 class Guard extends \Illuminate\Auth\Guard
 {
-    /** @var string[]|null */
+    /** @type string[]|null */
+    protected $roles;
+
+    /** @type string[]|null */
     protected $permissions;
 
     /**
@@ -21,14 +24,49 @@ class Guard extends \Illuminate\Auth\Guard
      */
     public function attempt(array $credentials = [], $remember = false, $login = true)
     {
-        if (!array_key_exists('namespaces', $credentials)) {
-            $credentials['namespaces'] = null;
+        if (!array_key_exists('namespace', $credentials)) {
+            $credentials['namespace'] = null;
         }
 
         return parent::attempt($credentials, $remember, $login);
     }
 
-    /** @noinspection ClassMethodNameMatchesFieldNameInspection */
+    /**
+     * Check if authenticate user has role
+     *
+     * @param string $role
+     *
+     * @return bool|null return NULL if user is not authenticated
+     */
+    public function hasRole($role)
+    {
+        if ($this->user() === null) {
+            return null;
+        }
+
+        return in_array($role, $this->roles, true);
+    }
+
+    /**
+     * Check if authenticated user has all roles passed by parameter
+     *
+     * @param string[] $roles
+     *
+     * @return bool|null return NULL if user is not authenticated
+     */
+    public function hasRoles($roles)
+    {
+        if ($this->user() === null) {
+            return null;
+        }
+
+        if (!is_array($roles)) {
+            $roles = func_get_args();
+        }
+
+        // Count roles because user need user to have all roles passed as parameter
+        return count(array_intersect($roles, $this->roles)) === count($roles);
+    }
 
     /**
      * Check if authenticated user has permission
@@ -47,6 +85,27 @@ class Guard extends \Illuminate\Auth\Guard
     }
 
     /**
+     * Check if authenticated user has all permission passed by parameter
+     *
+     * @param string[] $permissions
+     *
+     * @return bool|null return NULL if user is not authenticated
+     */
+    public function hasPermissions($permissions)
+    {
+        if ($this->user() === null) {
+            return null;
+        }
+
+        if (!is_array($permissions)) {
+            $permissions = func_get_args();
+        }
+
+        // Count permissions because user need user to have all permissions passed as parameter
+        return count(array_intersect($permissions, $this->permissions)) === count($permissions);
+    }
+
+    /**
      * {@inheritDoc}
      */
     public function user()
@@ -54,7 +113,8 @@ class Guard extends \Illuminate\Auth\Guard
         $user = parent::user();
 
         // Load roles if user is set first time
-        if ($user !== null && $this->permissions === null) {
+        if ($user !== null && ($this->roles === null || $this->permissions === null)) {
+            $this->roles       = [];
             $this->permissions = [];
 
             // Load roles only if User model is role based user
@@ -62,6 +122,7 @@ class Guard extends \Illuminate\Auth\Guard
                 // Load permissions from roles
                 $roles = $user->roles;
                 foreach ($roles as $role) {
+                    $this->roles[] = $role->token;
                     foreach ($role->permissions as $permission) {
                         $this->permissions[] = $permission->token;
                     }
@@ -78,23 +139,6 @@ class Guard extends \Illuminate\Auth\Guard
         }
 
         return $user;
-    }
-
-    /**
-     * Check if authenticated user has all permission passed by parameter
-     *
-     * @param string[] ...$permissions
-     *
-     * @return bool|null return NULL if user is not authenticated
-     */
-    public function hasPermissions(...$permissions)
-    {
-        if ($this->user() === null) {
-            return null;
-        }
-
-        // Count permissions because user need to have all permissions passed as parameter
-        return count(array_intersect($permissions, $this->permissions)) === count($permissions);
     }
 
     /**
